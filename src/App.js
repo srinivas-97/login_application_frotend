9a2125e4b193e6e6c4c331d57072e7188f4c069c
Srinivas <srinu781997@gmail.com>
2023-09-24 14:54:40 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+
+    const [data, setData] = useState({username:"", password:""});
+    const [isFormSubmitted, setFormSumbit] = useState(false);
+    const [showLoginError, setShowLoginError] = useState(false);
+
+
+    const loginForm = () => {
+
+      return(
+        <div className='login-container'>
+          <h1>Please Login</h1>
+          <div className="form-container">
+            <form className="form-login" onSubmit = {handleLoginForm}>
+              {showLoginError && <p className="error-message">Please check username and passowrd</p>}
+              <label>username:
+              <input name="username" type="text" onChange = {changeUserNameHandler}/></label>
+              <br/>
+              <label>password:
+              <input name="password" type="password" onChange = {changePasswordHandler}/></label>
+              <br/>
+              <button type="submit" className="submit-button">submit</button>
+            </form>
+          </div>
+          </div>
+          )
+    }
+
+
+
+    const successMessage = () => (<div className="success-message">Login Success</div>)
+
+    const handleLoginForm = async event => {
+      event.preventDefault()
+      const url = 'https://loginapplication-production.up.railway.app/login'
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      }
+      const response = await fetch(url, options);
+      if (response.ok === true) {
+        setFormSumbit(true);
+      } else {
+        setFormSumbit(false);
+        setShowLoginError(true)
+      }
+     
+    }
+
+    const changeUserNameHandler = (event) => {
+          setData({...data, username: event.target.value})
+    }
+    const changePasswordHandler = (event) => {
+      setData({...data, password: event.target.value})
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> {isFormSubmitted
+            ? successMessage()
+            : loginForm()}
+        
+      
+      </div>
     </div>
   );
 }
